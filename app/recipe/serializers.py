3c2678c0b78816740c12a90275248169f7803c70
igor albuquerque <igoralbuquerque102@hotmail.com>
2025-01-26 18:49:03 -0300

    adicionado endpoints para criar, deletar e atualizar tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,18 +3,42 @@ Serializers for recipe API
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import Recipe, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """serializer for tags"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name',)
+        read_only_fields = ('id',)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe objects"""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
         fields = (
-            'id', 'title', 'time_minutes', 'price', 'link',
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags',
         )
         read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        """create a recipe"""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerializer(RecipeSerializer):
