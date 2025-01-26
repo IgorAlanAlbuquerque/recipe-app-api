c47330aff4b3d721bc9a754c94a769c7df19325c
igor albuquerque <igoralbuquerque102@hotmail.com>
2025-01-26 15:47:33 -0300

    corrigido o teste para o endpoint de criar. Endpoint de criar funcionando

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -28,7 +28,7 @@ def create_recipe(user, **params):
     defaults = {
         'title': 'Sample recipe',
         'time_minutes': 10,
-        'price': Decimal(5.25),
+        'price': Decimal('5.25'),
         'description': 'Sample description',
         'link': 'http://example.com/recipe.pdf',
     }
@@ -101,17 +101,18 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(response.data, serializer.data)
 
     def test_create_recipe(self):
-        """test creating recipe"""
+        """Test creating a recipe."""
         payload = {
-            'title': 'Chocolate cheesecake',
+            'title': 'Sample recipe',
             'time_minutes': 30,
             'price': Decimal('5.99'),
+            'description': 'Sample description',
+            'link': 'http://example.com/recipe.pdf',
         }
-        create_recipe(user=self.user, **payload)
-        response = self.client.post(RECIPES_URL, payload)
+        res = self.client.post(RECIPES_URL, payload)
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        recipe = Recipe.objects.get(id=response.data['id'])
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
